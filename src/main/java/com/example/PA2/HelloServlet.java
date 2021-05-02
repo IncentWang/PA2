cb155e0d069f7c9628e1a717a4c7d772e51b1a6b
IncentWang <wwhincent@gmail.com>
2021-05-02 10:29:30 +0800

    Add pages from version 1.0 for easily access and code.

--- src/main/java/com/example/PA2/HelloServlet.java
+++ src/main/java/com/example/PA2/HelloServlet.java
@@ -3,13 +3,24 @@ package com.example.PA2;
 import java.io.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
+import java.sql.*;
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
     private String message;
+    Connection connection;
 
     public void init() {
-        message = "Hello World!";
+
+        // Set database address and the driver will be used
+        final String ADDRESS = "localhost:3306"; // TODO: change it before deploy to the server
+        final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+
+        // Set the username and the password of the database
+        final String USERNAME = "incent"; // TODO: change it before deploy to the server
+        final String PASSWORD = "incent";
+
+
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
