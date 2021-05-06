4b6b47d7d052c5b981f89e68b57f81d3a3adb9e7
IncentWang <wwhincent@gmail.com>
2021-05-06 11:16:02 +0800

    It can generate the product detail page correctly with having a button to add a product to the cart (button is untested, may not work well).

--- src/main/java/com/example/PA2/cartServlet.java
+++ src/main/java/com/example/PA2/cartServlet.java
@@ -5,11 +5,23 @@ import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 
+/**
+ * This Servlet is used to get the parameter from productServlet and put the data into a session attribute to log the things in the cart.
+ * NOTE: This servlet will not generate a cart page.
+ */
 @WebServlet(name = "cartServlet", value = "/cartServlet")
 public class cartServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        String model = request.getParameter("name");
+        HttpSession session = request.getSession();
+        if(session.getAttribute("name") == null) {
+            session.setAttribute(model, 1);
+        }
+        else{
+            session.setAttribute(model, Integer.valueOf(session.getAttribute("name").toString()) + 1);
+        }
+        response.getWriter().println("<head>Successful Add to Cart!</head>");
     }
 
     @Override
--- src/main/java/com/example/PA2/productServlet.java
+++ src/main/java/com/example/PA2/productServlet.java
@@ -4,16 +4,92 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.*;
 
 @WebServlet(name = "productServlet", value = "/productServlet")
 public class productServlet extends HttpServlet {
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = request.getParameter("model"); // IMPORTANT: This parameter named MODEL
 
+        PrintWriter out = response.getWriter();
+        out.print(initHtml());
+
+        Product product;
+
+
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306", "root", "incent");
+            Statement statement = con.createStatement();
+            String sql = "SELECT * FROM pa2.phone_information WHERE phone_name=" + "'" + name + "'";
+
+            System.out.println(sql);
+
+            ResultSet rs = statement.executeQuery(sql);
+            while(rs.next()) {
+                product = new Product(rs.getString(1), rs.getString(2),
+                        rs.getString(3), rs.getFloat(4), rs.getInt(5), rs.getInt(6), rs.getString(8));
+                out.print(generateDetail(product));
+            }
+        }
+        catch(ClassNotFoundException | SQLException e){
+            out.println("inside catch");
+            e.printStackTrace();
+        }
+        finally {
+            out.close();
+        }
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+    }
+
+    private String generateDetail(Product product)
+    {
+        String html = "";
+        html += "<h2>Product Description</h2>\n";
+        html += "<img class = \"product_image\" src=" + product.getImagePath() + " alt=\"Photo of phone\"/>\n";
+        html += "<b>" + product.getName() + "</b>\n";
+        html += "<p>Colors: " + product.getColors() + "</p>\n";
+        html += "<p>Prices: $" + product.getPrice() + "</p>\n";
+        html += "<p>Rating on this model: " + product.getRating() + "</p>\n";
+        html += "<button onclick=\"document.location='./cartServlet?name=" + product.getName() + "'\">Add To Cart</button>";
+        return html;
+    }
+
+    private String initHtml()
+    {
+        return "<!doctype html>\n" +
+                "\n" +
+                "\n" +
+                "<html lang=\"en\">\n" +
+                "<head>\n" +
+                "  <meta charset=\"utf-8\">\n" +
+                "  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n" +
+                "\n" +
+                "  <title>Product Description</title>\n" +
+                "  <link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css\">\n" +
+                "  <link rel=\"stylesheet\" href=\"stylesheets/store_style.css\">\n" +
+                "  <link rel=\"stylesheet\" href=\"stylesheets/navbar_style.css\">\n" +
+                "  <link rel=\"stylesheet\" href=\"stylesheets/product_description_style.css\">\n" +
+                "\n" +
+                "  <script type=\"text/javascript\" src=\"js/product_info.js\"></script>\n" +
+                "  <script type=\"text/javascript\" src=\"js/form_validation.js\"></script>\n" +
+                "\n" +
+                "</head>\n" +
+                "<ul>\n" +
+                "        <li><a href=\"index.html\">Home</a></li>\n" +
+                "        <li><a class=\"active\" href=\"store.html\">Store</a></li>\n" +
+                "        <li><a href=\"about.html\">About</a></li>\n" +
+                "</ul>\n";
     }
 }
