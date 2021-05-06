818893ab12cfc5806a90b155eabd23b0b76dcb01
IncentWang <wwhincent@gmail.com>
2021-05-07 01:17:06 +0800

    Fix the bug which cannot store the data to the database.

--- src/main/java/com/example/PA2/placeOrderServlet.java
+++ src/main/java/com/example/PA2/placeOrderServlet.java
@@ -19,17 +19,18 @@ public class placeOrderServlet extends HttpServlet {
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306", "root", "incent"); // TODO: Change it before deploying to the remote server.
-            PreparedStatement statement = con.prepareStatement("INSERT INTO orders(product_name, orderer_name, total_price) VALUES(?,?,?)");
+            PreparedStatement statement = con.prepareStatement("INSERT INTO orders (product_name, orderer_name, total_price) VALUES (?,?,?)");
+            Statement initedStatement = con.createStatement();
             statement.setString(1, request.getParameter("productNames"));
             statement.setString(2, request.getParameter("fname") + request.getParameter("lname"));
             statement.setFloat(3, Float.parseFloat(request.getParameter("price")));
-            System.out.println(statement.toString());
+            initedStatement.execute("use pa2");
             statement.execute();
 
 
         }catch(SQLException | ClassNotFoundException e)
         {
-            System.out.println("inside Catch");
+            System.out.println(e.getMessage());
         }finally{
             RequestDispatcher dispatcher = request.getRequestDispatcher("/");
             dispatcher.forward(request, response);
